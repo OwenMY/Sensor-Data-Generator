d7622496ae4659e5a949acef4e82146898e488d6
Owen Yoshishige <owenyoshishige@gmail.com>
2022-07-08 23:14:04 -0700

    set up intial webpack and babel settings

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 const router = require('./routes.js');
 const port = 3000;
 
 /* middleware */
+app.use(cors());
+app.use(express.static(path.join(__dirname, './../dist')));
 app.use(router);
 
 app.listen(port, () => {
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementsById('root'));
+root.render(<h1>Sensor Data Generator</h1>);
