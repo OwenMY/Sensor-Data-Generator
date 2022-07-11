cd7753c27f77f717114ffb8a0cf737e29ae74e5a
Owen Yoshishige <owenyoshishige@gmail.com>
2022-07-10 22:55:18 -0700

    add icons to home buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,16 +7,28 @@ const Home = () => {
       <h1>Sensor Data Generator</h1>
       <div className="button-ctr">
         <Link to="/calibrate-sensor">
-          <button className="btn" >Calibrate Sensors</button>
+          <button className="btn" >
+            <img src={require("./../../src/assets/sensor_sideways.svg")} alt="sensor image"></img>
+            <span>Calibrate Sensors</span>
+          </button>
         </Link>
         <Link to="/calibrate-base">
-          <button  className="btn">Calibrate Base</button>
+          <button  className="btn">
+            <img src={require("./../../src/assets/base.png")} alt="base Image"></img>
+            <span>Calibrate Base</span>
+          </button>
         </Link>
         <Link to="/validate-system">
-          <button className="btn">Validate System</button>
+          <button className="btn">
+            <img src={require("./../../src/assets/system.png")} alt="system image"></img>
+            <span>Validate System</span>
+          </button>
         </Link>
         <Link to="/search">
-          <button className="btn">Search</button>
+          <button className="btn">
+            <img src={require("./../../src/assets/search.png")} alt="search image"></img>
+            <span>Search</span>
+          </button>
         </Link>
       </div>
     </div>
