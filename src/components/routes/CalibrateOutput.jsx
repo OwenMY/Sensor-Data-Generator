150cce078b02ae7a188b63184c33b5db95e9c82c
Owen Yoshishige <owenyoshishige@gmail.com>
2022-07-12 13:52:47 -0700

    create draft of Output Table page

--- src/components/routes/CalibrateOutput.jsx
+++ src/components/routes/CalibrateOutput.jsx
@@ -2,14 +2,40 @@ import React from 'react';
 import {useState} from 'react';
 import {useLocation, Link} from 'react-router-dom';
 import axios from 'axios';
+import Loading from './../Loading.jsx'
+import CalOutputTable from './../tables/CalOutputTable.jsx';
 
 const CalibrateOutput = () => {
-  const [algorithm, setAlgorithm] = useState('');
+  const [output, setOutput] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const data = location.state;
 
-  const handleInput = (e) => {
-    //
+  const handleSubmit = () => {
+    let dropDown = document.getElementById('algorithms');
+    setIsLoading(true);
+    let algorithm = 'v1.2.1';
+
+    let sensors = data.sensors.map(sensor => {
+      return {
+        sensor_unqiue_id: sensor.sensor_unqiue_id
+      };
+    })
+
+    for (let i = 0; i < 5; i++) {
+      let child = dropDown.children[i];
+      if (child.selected) algorithm = child.value
+    }
+
+    let body = {
+      calibration_file: data.file_name,
+      algoritm_version: algorithm,
+      sensors: sensors
+    };
+
+    // axios.post('/', body)
+    //   .then(res => setOutput(res.data))
+    //   .catch(err => console.error(err));
   };
 
   return (
@@ -23,13 +49,13 @@ const CalibrateOutput = () => {
           <option value="v1.2.4">v1.2.4</option>
           <option value="v1.2.5">v1.2.5</option>
         </select>
-        <button type="submit">Create</button>
+        <button onClick={handleSubmit} type="submit">Create</button>
         <Link to="/">
           <button>Return</button>
         </Link>
       </div>
-      <div className="output-ctr"></div>
-    </div>
+      {isLoading ? <Loading /> : <CalOutputTable output={output}/>}
+   </div>
   );
 };
 
---
+++ src/components/tables/CalOutputTable.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const CalOutputTable = ({output}) => {
+  return !output.length ? null : (
+    <div>
+      poop
+    </div>
+  )
+};
+
+export default CalOutputTable;
