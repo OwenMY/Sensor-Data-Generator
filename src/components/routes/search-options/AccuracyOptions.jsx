eb338bbef3db71e25c34694bfc50b2fa50e0a967
Owen Yoshishige <owenyoshishige@gmail.com>
2022-07-13 17:50:01 -0700

    create Accuracy options

---
+++ src/components/routes/search-options/AccuracyOptions.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AccuracyOptions = ({dispatch}) => {
+  return (
+    <>
+      <label htmlFor="Accuracy">Accuracy</label>
+      <select
+        onChange={(e) => dispatch({type: 'accuracy', payload: e.target.value})}
+        className="dropdown"
+        name="Accuracy"
+      >
+        <option value="Choose Limit" defaultValue>Choose Limit</option>
+        <option value="< 0.1">{'< 0.1'}</option>
+        <option value="< 0.2">{'< 0.2'}</option>
+        <option value="< 0.3">{'< 0.3'}</option>
+        <option value="< 0.4">{'< 0.4'}</option>
+        <option value="< 0.5">{'< 0.5'}</option>
+        <option value="< 0.6">{'< 0.6'}</option>
+        <option value="< 0.7">{'< 0.7'}</option>
+        <option value="< 0.8">{'< 0.8'}</option>
+        <option value="< 0.9">{'< 0.9'}</option>
+        <option value="< 1.0">{'< 1.0'}</option>
+        <option value="outliers">{'outliers'}</option>
+      </select>
+    </>
+  );
+};
+
+export default AccuracyOptions;
